24a1864fd8b4b44f6da02ad0183bb0940decb33d
chunzhi23 <lzhongm05@gmail.com>
2025-03-29 22:17:54 +0900

    trivial correction

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import { TemplateProps } from "keycloakify/login/TemplateProps";
 import { KcContext } from "./kc.gen";
 import { I18n } from "./login/i18n";
+import { JSX } from "react";
 
 type CustomTemplateProps = Omit<TemplateProps<KcContext, I18n>, "doUseDefaultCss">;
 
